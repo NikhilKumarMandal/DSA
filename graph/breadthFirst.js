f8ee5691d23018bebd9cbddd81620270823d48a9
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-06-17 22:48:22 +0530

    breath first in graph

---
+++ graph/breadthFirst.js
@@ -0,0 +1,28 @@
+const graph = {
+    a: ["b", "c"],
+    b: ['a', 'c', 'd'],
+    c: ['a', 'b', 'd'],
+    d: ['b', 'c'],
+    e: ['f'],
+    f: ['e']
+}
+
+
+function breadthFirstIterative(graph, start) {
+    const queue = [start];
+    const visited = new Set();
+    visited.add(start);  
+
+    while (queue.length > 0) {
+        const node = queue.shift();
+        for (const neighbour of graph[node]) {  
+            if (!visited.has(neighbour)) { 
+                visited.add(neighbour);
+                queue.push(neighbour);
+            }
+        }
+    }
+    return visited;
+}
+
+console.log(breadthFirstIterative(graph, 'a')); 
